f59fdb1d23f8e88971d6bf2765bb36656e4e4447
Hareyabirhane <104759727+Hareyabirhane@users.noreply.github.com>
2022-06-25 16:43:52 +0300

    Update main.c

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -7,18 +7,4 @@ int main(void)
 _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
 return (0);
 }
-}
-​
-cc -std=gnu89 main.c -L. -lmy -o quote
-./quote 
-​
-
-
-
-
-
-git push
-
-
-
 
