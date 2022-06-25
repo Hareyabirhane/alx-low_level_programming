73f433abc008397316b5edefdd986fa71ca46ef1
Hareyabirhane <hareyatsibah@gmail.com>
2022-06-25 06:22:10 -0700

    add

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,24 @@
+/**
+ * main - Entry point
+ * Return: 0.
+ */
+int main(void)
+{
+_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+return (0);
+}
+}
+​
+cc -std=gnu89 main.c -L. -lmy -o quote
+./quote 
+​
+
+
+
+
+
+git push
+
+
+
+
